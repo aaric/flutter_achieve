2ca7a4c989109e7cb1df943941309e8f2265ccd5
aaric <vipaaric@gmail.com>
2019-05-18 22:36:12 +0800

    1.1.0 仿微信聊天示例

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,17 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  var appName = '仿微信聊天示例';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'App学习示例',
+      title: appName,
       theme: ThemeData(
         // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'App学习示例'),
+      home: MyHomePage(title: appName),
     );
   }
 }
@@ -28,91 +30,15 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var _selectedIndex = 1;
-  final _widgetOption = [
-    Text('Index 0: 信息'),
-    Text('Index 1: 通讯录'),
-    Text('Index 2: 发现'),
-    Text('Index 3: 我'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text('Aaric'),
-              accountEmail: Text('vipaaric@gmail.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/default_avatar.png'),
-              ),
-              otherAccountsPictures: <Widget>[
-                new Container(
-                  child: Image.asset('assets/images/default_qrcode.png'),
-                ),
-              ],
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.color_lens),
-              ),
-              title: Text('主题中心'),
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.photo),
-              ),
-              title: Text('个人相册'),
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                child: Icon(Icons.wifi),
-              ),
-              title: Text('免流量特权'),
-            ),
-          ],
-        ),
-      ),
       body: Center(
-        child: _widgetOption.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            title: Text('信息'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contacts),
-            title: Text('通讯录'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text('发现'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            title: Text('我'),
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-        showUnselectedLabels: true,
-        currentIndex: _selectedIndex,
-        unselectedItemColor: Colors.black54,
-        fixedColor: Theme.of(context).primaryColor,
-        onTap: _tappedOnItem,
+        child: Text('${widget.title}'),
       ),
     );
-  }
-
-  void _tappedOnItem(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
   }
 }

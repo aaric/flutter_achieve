a1a6602360c3ed972f7ceb38df174cee7865c43d
aaric <qlhan@incarcloud.com>
2020-02-11 15:02:36 +0800

    1.2.2 Scaffold

---
+++ lib/pages/bus_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class BusPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('BusPage'),
+    );
+  }
+}
+

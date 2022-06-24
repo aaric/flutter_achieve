c60e16ba41d464a2cca0e0aaf64f9dff51b405f5
qlhan <qlhan@incarcloud.com>
2022-06-24 17:58:43 +0800

    2.1.0 flutter v3

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue
       ),
-      home: const DemoPage(title: 'Flutter Demo Home Page'),
+      home: const DemoPage(title: 'Flutter Demo Home Page')
     );
   }
 }

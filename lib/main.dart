70282f6f58e555d94e12cab43a6a254b84ef9ba2
qlhan <qlhan@incarcloud.com>
2022-06-24 11:59:18 +0800

    2.1.0 flutter v3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 // import 'ch01_hello_word.dart';
 // import 'ch02_base_image.dart';
+// import 'ch03_base_info.dart';
 // import 'ex01_css_colors.dart';
 // import 'ex03_url_launcher.dart';
 // import 'ex04_mqtt_client.dart';
-import 'ch03_base_info.dart';
+import 'ch05_device_info_plus.dart';
 
 void main() {
   runApp(const MyApp());

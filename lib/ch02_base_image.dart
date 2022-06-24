4aa52e507d5dfb60646001e2dc3c387fa79fb837
qlhan <qlhan@incarcloud.com>
2022-06-24 12:50:20 +0800

    2.1.0 flutter v3

--- lib/ch02_base_image.dart
+++ lib/ch02_base_image.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_applet/ex04_mqtt_client.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DemoPage extends StatefulWidget {
+  const DemoPage({super.key, required this.title});
+
+  final String title;
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: DemoPage());
-  }
+  State<StatefulWidget> createState() => _DemoDemoPage();
 }
 
-class DemoPage extends StatelessWidget {
-  const DemoPage({super.key});
+class _DemoDemoPage extends State<DemoPage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
         body: Flex(
           direction: Axis.vertical,
           children: [
             Expanded(
               // flex: 1,
-              child: Image.asset('assets/images/banzhuan.jpg')
+                child: Image.asset('assets/images/banzhuan.jpg')
             ),
             Expanded(
               // flex: 2,
-              child: Image.network('http://10.0.11.25:8021/test/banzhuan.jpg')
+                child: Image.network('http://10.0.11.25:8021/test/banzhuan.jpg')
             )
           ],
         )
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'ch01_hello_word.dart';
-// import 'ch02_base_image.dart';
+// import 'ch01_hello_word.dart';
+import 'ch02_base_image.dart';
 // import 'ch03_base_info.dart';
 // import 'ex01_css_colors.dart';
 // import 'ex03_url_launcher.dart';

b0b4c41a09a9e9cf38c976e485457edba526d1eb
qlhan <qlhan@incarcloud.com>
2022-09-08 11:58:59 +0800

    2.3.0 flutter route

--- lib/ch08_base_route.dart
+++ lib/ch08_base_route.dart
@@ -18,7 +18,54 @@ class _DemoPageState extends State<DemoPage> {
         title: Text(widget.title)
       ),
       body: Center(
-        child: const Text('hello route')
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Goods: Ice Cream'),
+            Text('OrderId: 20220908001'),
+            ElevatedButton(onPressed: () async {
+              var payAmount = await Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return NewPage(orderId: '20220908001');
+                  }
+              ));
+              print('pay amount: $payAmount');
+            }, child: Text('pay money'))
+          ]
+        )
+      )
+    );
+  }
+}
+
+class NewPage extends StatefulWidget {
+  const NewPage({super.key, required this.orderId});
+
+  final String orderId;
+
+  @override
+  State<StatefulWidget> createState() => _NewPageState();
+}
+
+
+class _NewPageState extends State<NewPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Order Id: ${widget.orderId}')
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Amount: ￥ 10.00'),
+            ElevatedButton(onPressed: () {
+              Navigator.pop(context, '￥ 10.00');
+            }, child: Text('back result'))
+          ]
+        )
       )
     );
   }
